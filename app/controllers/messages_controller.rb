cef2ead90c380b1724732ec67b0539f3f3559d19
Jose Rivera <jriverac@me.com>
2014-07-22 17:37:48 -0500

    Cleanup, removing all the elements related to user interface and not needed by the API.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,17 +10,6 @@ class MessagesController < ApplicationController
     end
   end
 
-  # GET /messages/1
-  # GET /messages/1.json
-  def show
-    @message = Message.find(params[:id])
-
-    respond_to do |format|
-      #format.html # show.html.erb
-      format.json { render json: @message }
-    end
-  end
-
   # GET /messages/new
   # GET /messages/new.json
   def new
@@ -32,10 +21,6 @@ class MessagesController < ApplicationController
     end
   end
 
-  # GET /messages/1/edit
-  def edit
-    @message = Message.find(params[:id])
-  end
 
   # POST /messages
   # POST /messages.json
@@ -44,9 +29,6 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       if @message.save
-          puts ENV['EMAIL_DOMAIN']
-          puts ENV['EMAIL_USERNAME']
-          puts ENV['EMAIL_PASSWORD']
 
           MessageMailer.new_message(@message).deliver
 
@@ -59,31 +41,4 @@ class MessagesController < ApplicationController
     end
   end
 
-  # PUT /messages/1
-  # PUT /messages/1.json
-  def update
-    @message = Message.find(params[:id])
-
-    respond_to do |format|
-      if @message.update_attributes(params[:message])
-        #format.html { redirect_to @message, notice: 'Message was successfully updated.' }
-        format.json { head :no_content }
-      else
-        #format.html { render action: "edit" }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /messages/1
-  # DELETE /messages/1.json
-  def destroy
-    @message = Message.find(params[:id])
-    @message.destroy
-
-    respond_to do |format|
-      format.html { redirect_to messages_url }
-      format.json { head :no_content }
-    end
-  end
 end
