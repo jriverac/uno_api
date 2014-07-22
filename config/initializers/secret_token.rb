96f8521b8a03b5e9043749c2fe05a0f91eb21813
Jose Rivera <jriverac@me.com>
2014-07-22 13:48:57 -0500

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnoApi::Application.config.secret_token = '08de994f783db30a0127bc5c5a2a76d198f8d536cdbbdb6ac9a750832d2f384405e157244358d59dff2bc11fffd242f5b94e13d0da34889aba31a8edb7371d1d'
